3889881b274c5c18fa95515faaf3bfc69e886602
Rain Lawson <89234053+rainlawson@users.noreply.github.com>
2026-02-25 12:35:20 -0600

    init

---
+++ GoHang.go
@@ -0,0 +1,11 @@
+package main
+
+// Packages are "namespaces and compilation units"
+// Every file in Go belongs to a package
+
+import "fmt"
+
+// Function order does not matter, can put other funcs later
+func main() {
+	fmt.Println("Go Hangul")
+}
