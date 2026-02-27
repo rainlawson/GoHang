8118f980bc9cc6002c65506e0a814f235d25f1fc
Rain Lawson <89234053+rainlawson@users.noreply.github.com>
2026-02-27 13:00:57 -0600

    Window shows up using ebitengine

--- GoHang.go
+++ GoHang.go
@@ -5,10 +5,13 @@ package main
 
 import (
 	_ "embed"
-	"fmt"
+	"log"
 	"math/rand"
 	"strings"
 	"time"
+
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 )
 
 // This is a pragma to tell compiler to read these contents into the binary
@@ -20,11 +23,54 @@ import (
 var wordData string
 
 // Function order does not matter, can put other funcs later
-func main() {
+// func main() {
+// 	rand.Seed(time.Now().UnixNano())
+
+// 	words := strings.Split(strings.TrimSpace(wordData), "\n")
+
+// 	random := words[rand.Intn(len(words))]
+// 	fmt.Println(random)
+// }
+
+type Game struct {
+	word   string
+	width  int
+	height int
+}
+
+func NewGame(w, h int) *Game {
 	rand.Seed(time.Now().UnixNano())
 
 	words := strings.Split(strings.TrimSpace(wordData), "\n")
+	return &Game{
+		word:   words[rand.Intn(len(words))],
+		width:  w,
+		height: h,
+	}
+}
 
-	random := words[rand.Intn(len(words))]
-	fmt.Println(random)
+func (g *Game) Update() error {
+	return nil
+}
+
+func (g *Game) Draw(screen *ebiten.Image) {
+	ebitenutil.DebugPrint(screen, "Random word: "+g.word)
+}
+
+func (g *Game) Layout(outsideW, outsideH int) (int, int) {
+	return g.width, g.height // critical fix
+}
+
+func main() {
+	sw, sh := ebiten.Monitor().Size()
+
+	w := int(float64(sw) * 0.8)
+	h := int(float64(sh) * 0.8)
+
+	ebiten.SetWindowSize(w, h)
+	ebiten.SetWindowTitle("GoHang")
+
+	if err := ebiten.RunGame(NewGame(w, h)); err != nil {
+		log.Fatal(err)
+	}
 }
