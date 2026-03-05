4f113ccd99be75054d5a56fcc41994bcbffa2dfa
Rain Lawson <89234053+rainlawson@users.noreply.github.com>
2026-03-05 11:43:10 -0600

    Added help.txt

--- GoHang.go
+++ GoHang.go
@@ -5,6 +5,7 @@ package main
 
 import (
 	_ "embed"
+	"image"
 	"log"
 	"math/rand"
 	"strings"
@@ -14,6 +15,8 @@ import (
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 )
 
+// --- Globals ---
+
 // This is a pragma to tell compiler to read these contents into the binary
 // At compile time
 // Also words.txt came from https://github.com/dwyl/english-words/blob/master/words_alpha.txt
@@ -22,46 +25,38 @@ import (
 //go:embed words.txt
 var wordData string
 
-// Function order does not matter, can put other funcs later
-// func main() {
-// 	rand.Seed(time.Now().UnixNano())
+//go:embed help.txt
+var helpText string
 
-// 	words := strings.Split(strings.TrimSpace(wordData), "\n")
-
-// 	random := words[rand.Intn(len(words))]
-// 	fmt.Println(random)
-// }
-
+// "Game implements ebiten.Game interface. ebiten.Game has necessary functions for an Ebitengine game: Update, Draw and Layout"
 type Game struct {
-	word   string
-	width  int
-	height int
+	word    string
+	width   int
+	height  int
+	state   int
+	lastEsc time.Time
 }
 
-func NewGame(w, h int) *Game {
-	rand.Seed(time.Now().UnixNano())
+const (
+	menu = iota
+	help
+	game
+)
 
-	words := strings.Split(strings.TrimSpace(wordData), "\n")
-	return &Game{
-		word:   words[rand.Intn(len(words))],
-		width:  w,
-		height: h,
-	}
-}
+// "iota" make this equivalent to:
+// const Menu State = 0
+// const Help State = 1
+// const Gameplay State = 2
 
-func (g *Game) Update() error {
-	return nil
-}
+var startRect = image.Rect(300, 200, 500, 240)
+var helpRectR = image.Rect(300, 260, 500, 300)
+var quitRect = image.Rect(300, 320, 500, 360)
 
-func (g *Game) Draw(screen *ebiten.Image) {
-	ebitenutil.DebugPrint(screen, "Random word: "+g.word)
-}
+// --- Main ---
 
-func (g *Game) Layout(outsideW, outsideH int) (int, int) {
-	return g.width, g.height // critical fix
-}
-
+// Function order does not matter, can put other funcs later
 func main() {
+	// Using Ebitengine to draw window & run game
 	sw, sh := ebiten.Monitor().Size()
 
 	w := int(float64(sw) * 0.8)
@@ -73,4 +68,119 @@ func main() {
 	if err := ebiten.RunGame(NewGame(w, h)); err != nil {
 		log.Fatal(err)
 	}
+
+}
+
+// --- Create New Game ---
+
+// WARNING: Vibe Coded
+func NewGame(w, h int) *Game {
+	// Returning a pointer to a new Game struct
+	rand.Seed(time.Now().UnixNano())
+
+	words := strings.Split(strings.TrimSpace(wordData), "\n")
+	return &Game{
+		word:    words[rand.Intn(len(words))],
+		width:   w,
+		height:  h,
+		state:   menu,
+		lastEsc: time.Now(),
+	}
+}
+
+// --- For Ebitengine ---
+
+// "Defines (*Game).Update function, that is called every tick. Tick is a time unit for logical updating.
+// The default value is 1/60 [s], then Update is called 60 times per second by default"
+//
+// This is where most of the work happens
+func (g *Game) Update() error {
+	x, y := ebiten.CursorPosition()
+
+	if g.state == menu {
+		if ebiten.IsMouseButtonPressed(ebiten.MouseButtonLeft) {
+
+			if inside(x, y, startRect) {
+				println("Game")
+				g.state = game
+			}
+
+			if inside(x, y, helpRectR) {
+				println("Help")
+				g.state = help
+			}
+
+			if inside(x, y, quitRect) {
+				println("Quit")
+				return ebiten.Termination
+			}
+		}
+
+		if ebiten.IsKeyPressed(ebiten.KeyEscape) {
+			now := time.Now()
+
+			if now.Sub(g.lastEsc) > 2*time.Second {
+				println("ESC")
+				return ebiten.Termination
+			}
+		}
+	}
+
+	if g.state == help {
+		if ebiten.IsKeyPressed(ebiten.KeyEscape) {
+			now := time.Now()
+
+			if now.Sub(g.lastEsc) > 2*time.Second {
+				println("ESC")
+				g.state = menu
+				g.lastEsc = now
+			}
+		}
+	}
+
+	if g.state == game {
+		if ebiten.IsKeyPressed(ebiten.KeyEscape) {
+			now := time.Now()
+
+			if now.Sub(g.lastEsc) > 2*time.Second {
+				println("ESC")
+				g.state = menu
+				g.lastEsc = now
+			}
+		}
+	}
+
+	return nil
+}
+
+// Helper function to check if a point is inside a rectangle
+func inside(x, y int, r image.Rectangle) bool {
+	return x >= r.Min.X && x <= r.Max.X && y >= r.Min.Y && y <= r.Max.Y
+}
+
+// "Defines (*Game).Draw function, that is called every frame" "Takes an *ebiten.Image as an argument"
+//
+//	func (g *Game) Draw(screen *ebiten.Image) {
+//		ebitenutil.DebugPrint(screen, "Random word: "+g.word)
+//	}
+func (g *Game) Draw(screen *ebiten.Image) {
+	switch g.state {
+
+	case menu:
+		ebitenutil.DebugPrintAt(screen, "[ Start Game ]", 320, 210)
+		ebitenutil.DebugPrintAt(screen, "[ Help ]", 350, 270)
+		ebitenutil.DebugPrintAt(screen, "[ Quit ]", 350, 330)
+
+	case help:
+		ebitenutil.DebugPrint(screen, helpText)
+
+	case game:
+		ebitenutil.DebugPrint(screen, "Game running")
+		ebitenutil.DebugPrint(screen, "Random word: "+g.word)
+	}
+}
+
+// "Defines (*Game).Layout function. Layout accepts an outside size, which is a window size on desktop, and returns the game's logical screen size."
+func (g *Game) Layout(outsideW, outsideH int) (int, int) {
+	return g.width, g.height // Vibe coded
 }
